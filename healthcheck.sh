47528e19b1826970b06027cb9c6d010f8601055c
Ivaylo Novakov <inovakov@gmail.com>
2020-04-17 15:31:35 +0200

    Add the HEALTHCHECK to the `dev` image as well.

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-# Make sure that `pidof` is available
-if [[ -z $(which pidof) ]]; then
-  exit 1
-fi
-
 # Check if `socat` is running
 if [[ -z $(pidof socat) ]]; then
   exit 1
