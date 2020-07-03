3672e18fbe04628a969a2f861ba86f791040773b
Ivaylo Novakov <inovakov@gmail.com>
2020-07-03 11:48:21 +0200

    Prepare the log files, so we don't get stale output or errors from `tail`.

--- scripts/run.sh
+++ scripts/run.sh
@@ -14,6 +14,10 @@ END
 # Create the SIA_DATA_DIR if it doesn't already exist.
 mkdir -p $SIA_DATA_DIR
 
+# Prepare the log files. Wipe them, if they exist.
+echo "" >"$SIA_DATA_DIR/siad_stdout.log"
+echo "" >"$SIA_DATA_DIR/siad_errout.log"
+
 # Output the logs to stdout and stderr.
 # See https://stackoverflow.com/a/23550347/363293 for details.
 tail -qF "$SIA_DATA_DIR/siad_stdout.log" &
