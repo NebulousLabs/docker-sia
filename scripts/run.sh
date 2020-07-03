2f973e954c02d77792c366198b56642c82036f20
Ivaylo Novakov <inovakov@gmail.com>
2020-07-03 11:40:11 +0200

    Keep outputting the data to `stdout` and `stderr`. Make sure the dir exists.

--- scripts/run.sh
+++ scripts/run.sh
@@ -11,6 +11,14 @@ SIAD_CMD=$(cat <<-END
 END
 )
 
+# Create the SIA_DATA_DIR if it doesn't already exist.
+mkdir -p $SIA_DATA_DIR
+
+# Output the logs to stdout and stderr.
+# See https://stackoverflow.com/a/23550347/363293 for details.
+tail -qF "$SIA_DATA_DIR/siad_stdout.log" &
+>&2 tail -qF "$SIA_DATA_DIR/siad_errout.log" &
+
 # We are using `exec` to start `siad` in order to ensure that it will be run as
 # PID 1. We need that in order to have `siad` receive OS signals (e.g. SIGTERM)
 # on container shutdown, so it can exit gracefully and no data corruption can
